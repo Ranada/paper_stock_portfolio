885380d8937a1d20f750e379f7475c55d61c54d3
Neil Ranada <neil.ranada@gmail.com>
2023-03-04 11:22:25 -0800

    Update table styling

--- src/pages/components/Table.tsx
+++ src/pages/components/Table.tsx
@@ -1,7 +1,7 @@
 
 export default function Table() {
     return (
-      <table className="w-full table-fixed rounded-xl bg-white/10 p-40 text-white text-white hover:bg-white/20">
+      <table className="w-full table-fixed text-white">
         <thead>
           <tr className="text-left">
             <th>Ticker</th>
@@ -11,25 +11,25 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr className="hover:bg-white/20">
             <td>AAPL</td>
             <td>Apple</td>
             <td>$2.0T</td>
             <td>25%</td>
           </tr>
-          <tr>
+          <tr className="hover:bg-white/20">
             <td>MSFT</td>
             <td>Microsoft</td>
             <td>$1.7T</td>
             <td>25%</td>
           </tr>
-          <tr>
+          <tr className="hover:bg-white/20">
             <td>GOOGL</td>
             <td>Alphabet</td>
             <td>$1.5T</td>
             <td>25%</td>
           </tr>
-          <tr>
+          <tr className="hover:bg-white/20">
             <td>ABNB</td>
             <td>Airbnb</td>
             <td>$88B</td>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,34 +19,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container w-2/3 flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl text-left font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Paper <span className="text-[hsl(280,100%,70%)]">Stock</span> Portfolio
+        <div className="container flex w-2/3 flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-left text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Paper <span className="text-[hsl(280,100%,70%)]">Stock</span>{" "}
+            Portfolio
           </h1>
-          <div>
+          <div className="w-full rounded-xl bg-white/10 p-4">
             <Table />
-            {/* <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link> */}
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
